aa9c6ef283b39580fa66bc5fd19a9f888096d989
samzhuwj <samzhuwj@gmail.com>
2017-12-14 19:22:39 +0800

    table Results

---
+++ tables.py
@@ -0,0 +1,10 @@
+from flask_table import Table, Col
+
+
+class Results(Table):
+    id = Col('Id', show=False)
+    artist = Col('Artist')
+    title = Col('Title')
+    release_date = Col('Release Date')
+    publisher = Col('Publisher')
+    media_type = Col('Media')
